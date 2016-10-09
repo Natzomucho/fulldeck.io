5ea81a9545cd2dc09046152f4f8a9076f257fd37
Brian Winkers <bwinkers@gmail.com>
2016-10-08 19:51:51 -0700

    Working menus

--- src/routes/web.js
+++ src/routes/web.js
@@ -16,39 +16,17 @@ let pageData = {};
 /**
  * Define routes and handlers
  */
-router.get('/', homepage);
-router.get('/technology', technology);
-router.get('/legal', legal);
-router.get('/demo', demo);
-router.get('/join', join);
-router.get('/account', account);
-router.get('/view3', view3);
-router.get('/page/account', pageAccount);
+router.get('/', spa);
+router.get('/account', spa);
+
 router.get('/app-chrome', appChrome);
 router.get('/component/:type/:name', getComponent);
-router.get('/appage/:name', getAppage);
 
 module.exports = router;
 
 /**
  * Define the handlers views
  */
-
-function *getAppage() {
-    try{
-        var locale = this.getLocaleFromQuery();
-        responseData = {};
-        var hostname = this.request.hostname;
-        responseData.site = ar.site.getSiteData(hostname, rootDir);
-        responseData.layout = 'htmlImport';
-        console.log('HMMMM!!!  component/appage/' + this.params.name);
-        yield this.render('component/appage/account', responseData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
 function *getComponent() {
     try{
         var locale = this.getLocaleFromQuery();
@@ -78,50 +56,7 @@ function *appChrome(next) {
     }
 }
 
-function *view3(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('homepage', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('pages/homepage', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *pageAccount(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('homepage', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('pages/account', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *account(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        let pageData = {};
-        pageData.site = {};
-        pageData.site.name = siteName;
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('homepage', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        pageData.layout = 'htmlImport';
-        yield this.render('pages/account', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *homepage(next) {
+function *spa(next) {
     try{
         var locale = this.getLocaleFromQuery();
         pageData = {};
@@ -129,8 +64,6 @@ function *homepage(next) {
         pageData.site = ar.site.getSiteData(hostname, rootDir);
         console.log(pageData);
         pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('homepage', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
         yield this.render('pages/homepage', pageData);
     }
     catch(err){
@@ -138,54 +71,3 @@ function *homepage(next) {
     }
 }
 
-function *technology(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('technology', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('technology', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *legal(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('legal', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('legal', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *demo(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('demo', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('demo', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
-
-function *join(next) {
-    try{
-        var locale = this.getLocaleFromQuery();
-        pageData.shared = lx.loadShared(locale);
-        pageData.page = lx.loadPage('join', locale);
-        pageData.nav = lx.loadComponent('nav', locale);
-        yield this.render('join', pageData);
-    }
-    catch(err){
-        this.throw(err);
-    }
-}
